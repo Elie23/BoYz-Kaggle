c10b15f2669ef240a094a2085bcb068ebed1baca
Elie23 <37155809+Elie23@users.noreply.github.com>
2018-03-07 17:19:56 -0500

    Update Version1.py

--- Version1.py
+++ Version1.py
@@ -1,3 +1,24 @@
 #test
-import numpy as np
-i = 0
+import numpy   as np 
+from scipy import misc
+x_test = np.loadtxt("test_x.csv", delimiter=",")
+print 'c long en ta...'
+x = np.loadtxt("train_x.csv", delimiter=",") # load from text 
+print 'bar..'
+y = np.loadtxt("train_y.csv", delimiter=",") 
+print 'nak!'
+x_test = x_test.reshape(-1,64,64)
+x = x.reshape(-1, 64, 64) # reshape 
+y = y.reshape(-1, 1) 
+print x.reshape(-1,4096)
+
+from sklearn.svm import LinearSVC
+from sklearn.metrics import f1_score
+x = x.reshape(-1,4096)
+y = y.reshape(50000)
+# print x.reshape(-1,4096).shape, y.shape
+for i in np.logspace(-5,5,11):
+    C = i
+    svm = LinearSVC(C=C)
+    svm_fit = svm.fit(x, y).predict(x)
+    print C, f1_score(y,svm_fit, average='macro')
