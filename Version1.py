24a0c0d1db940f313ab7b03d786b2bec609b88b9
Elie23 <37155809+Elie23@users.noreply.github.com>
2018-03-07 21:29:12 -0500

    Update Version1.py

--- Version1.py
+++ Version1.py
@@ -7,21 +7,22 @@ x = np.loadtxt("train_x.csv", delimiter=",") # load from text
 print 'bar..'
 y = np.loadtxt("train_y.csv", delimiter=",") 
 print 'nak!'
-x_test = x_test.reshape(-1,64,64)
-x = x.reshape(-1, 64, 64) # reshape 
-y = y.reshape(-1, 1) 
-print x.reshape(-1,4096)
 
 from sklearn.svm import LinearSVC
 from sklearn.metrics import f1_score
-x = x.reshape(-1,4096)
-y = y.reshape(50000)
-# print x.reshape(-1,4096).shape, y.shape
-for i in np.logspace(-5,5,11):
+from sklearn.model_selection import train_test_split
+
+#DATA SPLIT BOIZ
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.20, random_state=42)
+print x_train.shape, y_train.shape, x_val.shape, y_val.shape
+
+#GRINDING
+for i in np.logspace(-10,-6,5):
     C = i
+    print C
     svm = LinearSVC(C=C)
-    svm_fit = svm.fit(x, y).predict(x)
-    print C, f1_score(y,svm_fit, average='macro')
+    svm_fit = svm.fit(x_train, y_train).predict(x_val)
+    print C, f1_score(y_val,svm_fit, average='macro')
 
 
     
